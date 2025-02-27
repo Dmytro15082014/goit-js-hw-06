e5ce3e69bb23ef4ca3b09a325b366d65d52d6d72
Dmytro Teren <leo20091986@gmail.com>
2025-02-27 22:30:55 +0200

    tasks 1-2 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,39 @@
-`use strict`
+"use strict"
+
+class StringBuilder {
+
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padStart(str) {
+
+  }
+
+  padEnd(str) {
+    
+  }  
+
+  padBoth(str) {
+    
+  }
+};
+
+const builder = new StringBuilder(".");
+
+console.log(builder.getValue()); // "."
+
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
